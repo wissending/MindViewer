e7e6a135505ea23d8341ac033e87077e33b2cc84
JackeyLea <1768478912@qq.com>
2019-12-03 12:51:46 +0800

    设置软件图标

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle(QObject::tr("MindViewer"));
+    w.setWindowIcon(QIcon(":/resources/icons/mind.svg"));
     w.show();
     return a.exec();
 }
