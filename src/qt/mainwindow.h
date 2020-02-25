efa700c34657de304a9127de58dc4294f139f44a
JackeyLea <1768478912@qq.com>
2020-02-25 23:29:52 +0800

    功能完善

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -15,14 +15,40 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+struct pkgPacket{
+    int signal=0;
+
+    int delta=0;
+    int theta=0;
+    int lowAlpha=0;
+    int highAlpha=0;
+    int lowBeta=0;
+    int highBeta=0;
+    int lowGamma=0;
+    int midGamma=0;
+    int attention=0;//0-100
+    int meditation=0;//0-100
+    int rawdata=0;
+    int blink=0;
+
+    bool valid=false;//此包数据是否有效
+};//一个数据包包含的所有值
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    int size;//数据字符数
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    QString toStandardText(QString source);
+
+    QList<pkgPacket> parserData(QList<int> data);
+    QList<pkgPacket> parserData(QByteArray data);
 private slots:
     void on_actionOpen_triggered();
 
